34279d40fe98b83d330f6b75d93489d7bbab8c71
Marcos Morelli <marcos.eduardo.morelli@gmail.com>
2025-09-06 19:27:24 -0300

    v2

---
+++ README.md
@@ -0,0 +1,7 @@
+# Anisble MacOS
+
+## Steps:
+  1. Login at AppStore
+  2. `./init.sh`
+
+## Anisble Vault
---
+++ init.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+if xpath=$( xcode-select --print-path ) &&
+  test -d "${xpath}" && test -x "${xpath}" ; then
+  echo "Xcode Installed"
+else
+  xcode-select --install
+fi
+
+which -s brew
+if [[ $? != 0 ]] ; then
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+else
+  echo "Brew Installed"
+fi
+
+which -s pipx
+if [[ $? != 0 ]] ; then
+  brew install pipx
+  pipx ensurepath
+else
+  echo "Pipx Installed"
+fi
+
+which -s ansible
+if [[ $? != 0 ]] ; then
+  pipx install --include-deps ansible
+else
+  echo "Ansible Installed"
+fi
+
+if [ -f ./vault.pass ]; then
+    ansible-playbook main.yml --ask-become-pass --vault-password-file ./vault.pass
+else
+    ansible-playbook main.yml --ask-become-pass --ask-vault-pass
+fi
